a34c680c934fbf907bc735c3a870433cc23e6a1e
Thomas Freedman <tfreedman@thecomingbitsharesrevolution.website>
2018-03-22 21:47:44 -0500

    Bug: nextKey initialization
    An artifact from moving initialization back into function out of __main__

--- missedBlockSwitcher.py
+++ missedBlockSwitcher.py
@@ -75,7 +75,7 @@ def checkWitness():
         counterOnLastMiss = loopCounter = 0
         for key in range(len(WITNESS_KEYS)):
             if status['signing_key'] == WITNESS_KEYS[key]:
-                nextKey = key
+                nextKey = (key + 1) % len(WITNESS_KEYS)
 
     print("\r%d samples, missed=%d(%d), key=%.16s..." %
         (loopCounter, missed, counterOnLastMiss, currentKey), end='')
